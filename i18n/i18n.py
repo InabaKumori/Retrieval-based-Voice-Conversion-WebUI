98c68b9d96971fb0acd4e5c6adc4f043415754fa
WereIYou <51642034+InabaKumori@users.noreply.github.com>
2024-05-22 22:31:11 +0800

    Update i18n.py

--- i18n/i18n.py
+++ i18n/i18n.py
@@ -1,27 +1,18 @@
 import json
-import locale
 import os
 
-
 def load_language_list(language):
-    with open(f"./i18n/locale/{language}.json", "r", encoding="utf-8") as f:
+    with open(f"./i18n/{language}.json", "r", encoding="utf-8") as f:
         language_list = json.load(f)
     return language_list
 
-
 class I18nAuto:
-    def __init__(self, language=None):
-        if language in ["Auto", None]:
-            language = locale.getdefaultlocale()[
-                0
-            ]  # getlocale can't identify the system's language ((None, None))
-        if not os.path.exists(f"./i18n/locale/{language}.json"):
-            language = "en_US"
-        self.language = language
-        self.language_map = load_language_list(language)
+    def __init__(self, language=None):  # Ignore the language argument
+        self.language = "en_US"  # Force en_US
+        self.language_map = load_language_list(self.language)  
 
     def __call__(self, key):
         return self.language_map.get(key, key)
 
-    def __repr__(self):
-        return "Use Language: " + self.language
+    def print(self):
+        print("Use Language:", self.language)  # Will always print "en_US"
